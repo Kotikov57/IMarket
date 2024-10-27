6ce1832d24d4e975efa988d26e12ba5847d0f3c7
stevengu <stevengu@student.21-school.ru>
2024-10-27 22:12:34 +0300

    continue work

--- sql/data.sql
+++ sql/data.sql
@@ -17,6 +17,20 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Data for Name: products; Type: TABLE DATA; Schema: imarket_schema; Owner: postgres
+--
+
+COPY imarket_schema.products (id, name, price, quantity) FROM stdin;
+1	Utyug	1199.99	12
+2	Holodilnik	5000	20
+3	Chainik	1500.1	15
+4	Noutbuk	9999.99	50
+5	Myshka	200	200
+6	Klaviatura	100	100
+\.
+
+
+--
 -- Data for Name: orders; Type: TABLE DATA; Schema: imarket_schema; Owner: postgres
 --
 
@@ -30,15 +44,38 @@ COPY imarket_schema.orders (id, product_id, quantity, status) FROM stdin;
 
 
 --
--- Data for Name: products; Type: TABLE DATA; Schema: imarket_schema; Owner: postgres
+-- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY imarket_schema.products (id, name, price, quantity) FROM stdin;
-1	Utyug	1199.99	12
-2	Holodilnik	5000	20
+COPY public.orders (id, product_id, quantity, status) FROM stdin;
+2	10	100000	shipped
+1	1	1	pending
+4	4	4	cancelled
+5	1	5	delivered
+3	3	3	cancelled
+6	1	10	shipped
+\.
+
+
+--
+-- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.products (id, name, price, quantity) FROM stdin;
 3	Chainik	1500.1	15
 4	Noutbuk	9999.99	50
 5	Myshka	200	200
+6	Klaviatura	100	100
+2	Sosiska	3	1000000
+1	Utyug	1199.99	2
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, name, password) FROM stdin;
 \.
 
 
--- sql/schema.sql
+++ sql/schema.sql
@@ -25,6 +25,22 @@ CREATE SCHEMA imarket_schema;
 
 ALTER SCHEMA imarket_schema OWNER TO postgres;
 
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+ALTER SCHEMA public OWNER TO postgres;
+
+--
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON SCHEMA public IS '';
+
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -58,11 +74,107 @@ CREATE TABLE imarket_schema.products (
 ALTER TABLE imarket_schema.products OWNER TO postgres;
 
 --
+-- Name: orders; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.orders (
+    id integer,
+    product_id integer,
+    quantity integer,
+    status character varying(255)
+);
+
+
+ALTER TABLE public.orders OWNER TO postgres;
+
+--
+-- Name: products; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.products (
+    id integer,
+    name character varying(255),
+    price double precision,
+    quantity integer
+);
+
+
+ALTER TABLE public.products OWNER TO postgres;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name character varying(255),
+    password character varying(255)
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
 -- Name: products products_pk; Type: CONSTRAINT; Schema: imarket_schema; Owner: postgres
 --
 
 ALTER TABLE ONLY imarket_schema.products
     ADD CONSTRAINT products_pk PRIMARY KEY (id);
+
+
+--
+-- Name: products unique_id; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT unique_id UNIQUE (id);
+
+
+--
+-- Name: orders unique_orders_id; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT unique_orders_id UNIQUE (id);
+
+
+--
+-- Name: products unique_products_id; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT unique_products_id UNIQUE (id);
+
+
+--
+-- Name: products unique_products_name; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT unique_products_name UNIQUE (name);
+
+
+--
+-- Name: users users_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pk PRIMARY KEY (id);
+
+
+--
+-- Name: orders orders_orders__fk; Type: FK CONSTRAINT; Schema: imarket_schema; Owner: postgres
+--
+
+ALTER TABLE ONLY imarket_schema.orders
+    ADD CONSTRAINT orders_orders__fk FOREIGN KEY (product_id) REFERENCES imarket_schema.products(id);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE USAGE ON SCHEMA public FROM PUBLIC;
 
 
 --
